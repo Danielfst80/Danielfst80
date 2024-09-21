9d3898c0ac66eec613d48514e6575b9dcd5b90dc
Danielfst80 <143042687+Danielfst80@users.noreply.github.com>
2024-09-21 03:10:59 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '¡Hola, este es el Bot de Salvaterra!'
+
+@app.route('/bot', methods=['POST'])
+def bot_response():
+    incoming_msg = request.values.get('Body', '').lower()
+    response = ""
+    
+    if 'hola' in incoming_msg:
+        response = "¡Hola! ¿Cómo te puedo ayudar hoy?"
+    else:
+        response = "Lo siento, no entiendo eso."
+    
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
